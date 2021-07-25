c773a0ffa2a67ee2ca8c13b9d7dc78abbb1de0dd
K Muni Prudhvi <85389980+Prudhvi-2001@users.noreply.github.com>
2021-07-25 16:36:54 +0530

    Add files via upload

---
+++ Task2/src/content/Content1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../containers/styles.css';
+
+class data extends Component {
+    render() { 
+        return (  
+            <div className="data">
+                <h1>HWLDUHJDCHBJ</h1>
+            </div>
+        );
+    }
+}
+ 
+export default data;
